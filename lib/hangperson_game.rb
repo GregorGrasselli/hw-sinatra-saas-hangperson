70017401aa8c81333e5d900d4c352dfd9a03a58a
Gregor Grasselli <gregor.grasselli@gmail.com>
2016-06-20 21:45:01 +0000

    finished the HangpersonGame class. all tests passed

--- lib/hangperson_game.rb
+++ lib/hangperson_game.rb
@@ -7,16 +7,47 @@ class HangpersonGame
 
   # def initialize()
   # end
-  
-  def initialize(word)
+
+  def initialize word
     @word = word
+    @guesses = ''
+    @wrong_guesses = ''
   end
+
+  attr_accessor :word, :guesses, :wrong_guesses
 
   def self.get_random_word
     require 'uri'
     require 'net/http'
     uri = URI('http://watchout4snakes.com/wo4snakes/Random/RandomWord')
     Net::HTTP.post_form(uri ,{}).body
+  end
+
+  def guess l
+    raise ArgumentError if l.nil?
+    throw ArgumentError unless l.match(/[a-z]/i)
+    l.downcase!
+    return false if @guesses.include?(l) || @wrong_guesses.include?(l)
+    if @word.include? l
+      @guesses << l
+    else
+      @wrong_guesses << l
+    end
+  end
+
+  def word_with_guesses
+    regex_string = @guesses.empty? ? '.' : "[^#{@guesses}]"
+    @word.gsub Regexp.new(regex_string, true), '-'
+  end
+
+  def check_win_or_lose
+    if word_with_guesses == @word
+      return :win
+    elsif @wrong_guesses.length == 7
+      return :lose
+    else
+      return :play
+    end
   end
 
 end
